44f536d9008d80b1d8ed2469f8de5bf35f1489b1
sumit-krk <kushakk007@gmail.com>
2022-10-01 00:19:41 +0530

    Build Headrer in food-meal apps

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css'
+
+const Header=()=>{
+    return <>
+        <header className={classes.header}>
+            <h1>React Meals</h1>
+            <button>Cart</button>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt='food-item'/>
+        </div>
+    </>
+}
+export default Header;
